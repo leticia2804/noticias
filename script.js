7ca9984b00eab3df1789b83a9a7cdc03685faa3c
leticia2804 <135224747+leticia2804@users.noreply.github.com>
2024-03-15 09:04:10 -0300

    Update script.js
    commits não estão funcionando

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-let nome = document.querySelector ("h1")
+const botaoabrir = document.querySelector ("header > button ")
+const botaofechar = document.querySelector ("header nav button")
 
-console.log (nome)
+botaoabrir.addEventListener("click", abrirmenu)
+botaofechar.addEventListener("click", fecharmenu)
+
+const body = document.querySelector ("body")
+const nav = document.querySelector ("header.mobile")
+
+function abrirmenu() {
+    body.classList.add("escurecer")
+    nav.classList.add("abrir")
+}
+
+function fecharmenu () {
+    body.classList.add ("escurecer")
+    nav.classList.add ("abrir")
+}
